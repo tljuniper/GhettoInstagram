684585122c034f2b8dd3c9bc49a0fd0956d2cff8
Michael Schneider <michael.schneider@cyberus-technology.de>
2022-07-05 09:48:21 +0200

    add flask main

---
+++ src/main.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+# to execute run `FLASK_APP=main.py FLASK_ENV=DEBUG flask run`
+
+from flask import Flask
+from flask import request
+from flask import Response
+app = Flask(__name__)
+
+
+@app.route('/users/<username>/photos',methods = ['PUT'])
+def image_upload(username):
+  print(username)
+  data = request.form
+  return Response(status=200)
+
+@app.route('/feed',methods = ['GET'])
+def feed():
+  user = request.args.get('users')
+  return Response(status=200)
